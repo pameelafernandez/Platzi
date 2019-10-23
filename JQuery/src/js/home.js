2c605d310c252a3cb9908d1bd06e5516349de356
Fernandez <pamela.b.fernandez@accenture.com>
2019-10-23 17:06:11 -0300

    cambios home.js

--- JQuery/src/js/home.js
+++ JQuery/src/js/home.js
@@ -1,2 +1,34 @@
-console.log('hola mundo!');
- $("div").append("<")
+const getUSerAll = new Promise(function(todoBien, todoMal) {
+    // llamar a una api
+    setTimeout(function() {
+    // luego de 3 segundos
+        todoMal('se acabó el tiempo');
+    }, 5000)
+})
+
+const getUSer = new Promise(function(todoBien, todoMal) {
+    // llamar a una api
+    setTimeout(function() {
+    // luego de 3 segundos
+        todoBien('se acabó el tiempo');
+    }, 3000)
+})
+
+getUSer
+    .then(function(){
+        console.log('todo está bien')
+    })
+    .catch(function(message) {
+        console.log(message)
+    })
+
+Promise.all([
+    getUSer,
+    getUSerAll,
+])
+.then(function(message){
+    console.log(message)
+})
+.catch(function(message) {
+    console.log(message)
+})
